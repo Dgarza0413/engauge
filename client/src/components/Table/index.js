c3a0ddc8d0a908c5568577b5f694c9213a107e50
Christina Truong <christinaqtruong@gmail.com>
2019-08-05 14:57:13 -0500

    created table for the well table page using react bootstrap

--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -1,68 +1,40 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import { borderRight } from '@material-ui/system';
+import React from "react";
+import Table from 'react-bootstrap/Table'
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        display: 'flex',
-        float: borderRight,
-        marginTop: theme.spacing(3),
-        overflowX: 'auto',
-    },
-    table: {
-        minWidth: 650,
-    },
-}));
 
-function createData(Name, API, Flow, Disposal, Status) {
-    return { Name, API, Flow, Disposal, Status };
-}
-
-const rows = [
-    createData('Name', 159, 6.0, 24, 4.0),
-    createData('API', 237, 9.0, 37, 4.3),
-    createData('Flow', 262, 16.0, 24, 6.0),
-    createData('Disposal', 305, 3.7, 67, 4.3),
-    createData('Status', 356, 16.0, 49, 3.9),
-];
-
-function SimpleTable() {
-    const classes = useStyles();
-
+function WellTable() {
     return (
-        <Paper className={classes.root}>
-            <Table className={classes.table}>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Name</TableCell>
-                        <TableCell align="right">API No.</TableCell>
-                        <TableCell align="right">Flow</TableCell>
-                        <TableCell align="right">Disposal</TableCell>
-                        <TableCell align="right">Status</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map(row => (
-                        <TableRow key={row.name}>
-                            <TableCell component="th" scope="row">
-                                {row.Name}
-                            </TableCell>
-                            <TableCell align="right">{row.API}</TableCell>
-                            <TableCell align="right">{row.Flow}</TableCell>
-                            <TableCell align="right">{row.Disposal}</TableCell>
-                            <TableCell align="right">{row.Status}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </Paper>
+
+<Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td colSpan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</Table>
     );
 }
 
-export default SimpleTable;
+export default WellTable;
