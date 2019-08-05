11df173c94a2c283732ee319e9f117c79602ea81
David Garza <dgarza0413@gmail.com>
2019-08-05 12:21:20 -0500

    init files

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,44 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import DashBoard from "./pages/DashBoard";
+import WellTable from "./pages/WellTable";
+import WellReport from "./pages/WellReport";
+import WellDetail from "./pages/WellDetail";
+import NewWellForm from "./pages/NewWellForm";
+import Map from "./pages/Map";
+import Drawer from "./components/Drawer";
+import Container from "@material-ui/core/Container";
 
 function App() {
+  const styles = {
+    layout: {
+      display: "flex"
+    }
+  }
+
   return (
     <Router>
-      <div>
-        <Nav />
+      <div style={styles.layout}>
+        <Drawer />
         <Switch>
-          <Route exact path="/" component={Books} />
-          <Route exact path="/books/:id" component={Detail} />
+          <Container>
+            <Route exact path="/" component={Books} />
+            <Route exact path="/dashboard" component={DashBoard} />
+            <Route exact path="/map" component={Map} />
+            <Route exact path="/reports" component={WellReport} />
+            <Route exact path="/books/:id" component={Detail} />
+            <Route exact path="/welltable" component={WellTable} />
+            <Route exact path="/welltable/:id" component={WellDetail} />
+            <Route exact path="/newwell" component={NewWellForm} />
+          </Container>
           <Route component={NoMatch} />
+
         </Switch>
       </div>
-    </Router>
+    </Router >
   );
 }
 
---
+++ client/src/pages/WellTable.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Table from "../components/Table";
+
+class WellTable extends React.Component {
+    render() {
+        return (
+            <Table />
+        )
+    }
+}
+
+export default WellTable;
