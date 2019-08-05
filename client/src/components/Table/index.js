9a21f45d055749c639bc7bf69700776f49d5afb7
Christina Truong <christinaqtruong@gmail.com>
2019-08-05 15:11:56 -0500

    got table to populate data

--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -8,29 +8,20 @@ function WellTable() {
 <Table striped bordered hover>
   <thead>
     <tr>
-      <th>#</th>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Username</th>
+      <th>Well Name</th>
+      <th>API Number</th>
+      <th>Flow</th>
+      <th>Disposal</th>
+      <th>Status</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td colSpan="2">Larry the Bird</td>
-      <td>@twitter</td>
+      <td>Well 1</td>
+      <td>123-45-1235</td>
+      <td>Plunger Lift</td>
+      <td>Down the drain</td>
+      <td>On</td>
     </tr>
   </tbody>
 </Table>
