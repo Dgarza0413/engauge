cd43829ef1e20a5747c4418cc80e2dc7c78bcdae
Brittany McNulty <Brittany.m.mcnulty@gmail.com>
2019-08-05 22:22:07 -0500

    server page set up for oauth

--- client/src/App.js
+++ client/src/App.js
@@ -12,86 +12,86 @@ import Map from "./pages/Map";
 import Drawer from "./components/Drawer";
 import Container from "@material-ui/core/Container";
 import axios from 'axios'
-class App extends React.Component {
-  state = {
-    email:'test@test.com',
-    password:'testtest',
-    welcomeEmail: "",
-    googleSigninUrl: ""
-  }
+// class App extends React.Component {
+//   state = {
+//     email:'test@test.com',
+//     password:'testtest',
+//     welcomeEmail: "",
+//     googleSigninUrl: ""
+//   }
 
-  handleInput = event => {
-    const {name,value} = event.target
-    this.setState({
-      [name]: value
-    })
-  }
+//   handleInput = event => {
+//     const {name,value} = event.target
+//     this.setState({
+//       [name]: value
+//     })
+//   }
 
-  // let's try and login
+//   // let's try and login
 
-  handleFormSubmit = event => {
-    event.preventDefault()
-    const {email,password} = this.state
-    axios.post("/api/login", {email, password})
-      .then(result => {
-        console.log(result.data)
-        this.loadProfileInfo()
-      })
-  }
-  handleFormLogout = event => {
-    event.preventDefault()
-    axios.get("/api/logout")
-      .then(result => {
-        console.log(result.data)
-        this.setState({ welcomeEmail: ""})
-      })
-  }
+//   handleFormSubmit = event => {
+//     event.preventDefault()
+//     const {email,password} = this.state
+//     axios.post("/api/login", {email, password})
+//       .then(result => {
+//         console.log(result.data)
+//         this.loadProfileInfo()
+//       })
+//   }
+//   handleFormLogout = event => {
+//     event.preventDefault()
+//     axios.get("/api/logout")
+//       .then(result => {
+//         console.log(result.data)
+//         this.setState({ welcomeEmail: ""})
+//       })
+//   }
 
-  loadProfileInfo = () => {
-    axios.get('/api/user/me')
-      .then(response => {
-        this.setState({welcomeEmail: response.data.email})
-      })
-      .catch(err => {
-        axios.get("/api/google/url").then(response => {
-          this.setState({googleSigninUrl: response.data.url})
-        })
-      })
-  }
+//   loadProfileInfo = () => {
+//     axios.get('/api/user/me')
+//       .then(response => {
+//         this.setState({welcomeEmail: response.data.email})
+//       })
+//       .catch(err => {
+//         axios.get("/api/google/url").then(response => {
+//           this.setState({googleSigninUrl: response.data.url})
+//         })
+//       })
+//   }
 
-  componentDidMount(){
-    // Mostly just for developing locally
-    if(window.location.pathname === "/api/google/callback"){
-      const searchParams = new URLSearchParams(window.location.search);
-      axios.post("/api/google/code", {code: searchParams.get('code')}).then(() => {
-        window.location.href = "/"
-      })
-    } else {
-      this.loadProfileInfo()
-    }
-  }
+//   componentDidMount(){
+//     // Mostly just for developing locally
+//     if(window.location.pathname === "/api/google/callback"){
+//       const searchParams = new URLSearchParams(window.location.search);
+//       axios.post("/api/google/code", {code: searchParams.get('code')}).then(() => {
+//         window.location.href = "/"
+//       })
+//     } else {
+//       this.loadProfileInfo()
+//     }
+//   }
 
-  render(){
-    return (
-      <div>
-        <h1>{this.state.welcomeEmail.length > 0 
-          ? "Welcome " + this.state.welcomeEmail
-          : "Login"}</h1>
-          {
-            this.state.googleSigninUrl.length > 0 && this.state.welcomeEmail.length === 0
-            ? (<h3>Sign in with <a href={this.state.googleSigninUrl} >google </a></h3>)
-            : ""
-          }
-        <form>
-          <input onChange={this.handleInput} name="email" value={this.state.email} type="text"/>
-          <input onChange={this.handleInput} name="password" value={this.state.email} type="password"/>
-          <button onClick={this.handleFormSubmit}>Login</button>
-          <button onClick={this.handleFormLogout}>Logout</button>
-        </form>
-      </div>
-    );
-  }
-}
+//   render(){
+//     return (
+//       <div>
+//         <h1>{this.state.welcomeEmail.length > 0 
+//           ? "Welcome " + this.state.welcomeEmail
+//           : "Login"}</h1>
+//           {
+//             this.state.googleSigninUrl.length > 0 && this.state.welcomeEmail.length === 0
+//             ? (<h3>Sign in with <a href={this.state.googleSigninUrl} >google </a></h3>)
+//             : ""
+//           }
+//         <form>
+//           <input onChange={this.handleInput} name="email" value={this.state.email} type="text"/>
+//           <input onChange={this.handleInput} name="password" value={this.state.email} type="password"/>
+//           <button onClick={this.handleFormSubmit}>Login</button>
+//           <button onClick={this.handleFormLogout}>Logout</button>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
 
 function App() {
   const styles = {
--- server.js
+++ server.js
@@ -1,9 +1,40 @@
-const express = require("express");
-
-const mongoose = require("mongoose");
+require('dotenv').config()
+const express = require('express')
+const session = require('express-session')
+const mongoose = require('mongoose')
+const path = require('path')
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
+const passport = require('./config/passport.js')
+const { google } = require("googleapis")
+
+const db = require("./models")
+
+const googleConfig = {
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    redirect: process.env.GOOGLE_REDIRECT_URI
+}
+
+const defaultScope = [
+    'https://www.googleapis.com/auth/userinfo.email'
+]
+
+function createConnection(){
+    return new google.auth.OAuth2(
+        googleConfig.clientId,
+        googleConfig.clientSecret,
+        googleConfig.redirect
+    )
+}
+function getConnectionUrl(){
+    return createConnection().generateAuthUrl({
+        access_type: 'offline',
+        prompt: 'consent',
+        scope: defaultScope
+    })
+}
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,8 +43,143 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.use(session({secret: process.env.SESSION_SECRET || "the cat ate my keyboard", resave: true, saveUninitialized: true}))
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Add routes, both API and view
 app.use(routes);
+
+// API ROUTES GO HERE
+app.post('/api/login', passport.authenticate("local"),  (req, res) => {
+  res.json(req.user)
+})
+app.get('/api/logout', (req, res) => {
+  req.logout();
+  res.sendStatus(200);
+});
+
+app.get('/api/user/me', function(req, res){
+  if(req.user){
+      res.json({
+          email: req.user.email
+      })
+  } else {
+      res.sendStatus(401)
+  }
+  
+})
+
+app.get('/api/google/url', (req, res) => {
+  res.json({url: getConnectionUrl()})
+})
+
+function getGoogleAccountFromCode(code){
+  console.log("CODE");
+  console.log(code);
+  return createConnection().getToken(code).then(data => {
+      console.log("DATA");
+      console.log(data.tokens)
+      return Promise.resolve(data.tokens)
+  })
+}
+
+app.post('/api/google/code', (req, res) => {
+  const { code } = req.body;
+  getGoogleAccountFromCode(code).then(tokens => {
+      console.log(tokens)
+      const userConnection = createConnection()
+      userConnection.setCredentials(tokens)
+      userConnection.getTokenInfo(tokens.access_token).then(data => {
+          console.log("TOKEN INFO");
+          console.log(data);
+          const {email, sub } = data;
+
+          db.User.findOne({ email }).then(dbUser => {
+              if(!dbUser){
+                  // create a new user!
+                  db.User.create({
+                      email,
+                      authType: "google",
+                      googleId: sub
+                  }).then(finalDbUser => {
+                      req.login(finalDbUser, () => {
+                          res.json(true)
+                      })
+                  }).catch(err => {
+                      console.log(err)
+                      res.sendStatus(500)
+                  })
+
+              } else {
+                  // Check the type and googleId
+                  // if it matches, great! Login the user!
+                  // if not, something odd is up, reject it
+                  console.log(dbUser);
+                  if(dbUser.authType === "google" && dbUser.googleId === sub + ""){
+                      req.login(dbUser, () => {
+                          res.json(true)
+                      });
+                      
+                  } else {
+                      res.sendStatus(500)
+                  }
+              }
+          })
+
+      }).catch(() => {
+          res.sendStatus(500)
+      })
+  })
+})
+
+app.get('/api/google/callback', function(req, res){
+  const code = req.query.code
+  getGoogleAccountFromCode(code).then(tokens => {
+      const userConnection = createConnection()
+      userConnection.setCredentials(tokens)
+      userConnection.getTokenInfo(tokens.access_token).then(data => {
+          const {email, sub } = data;
+          db.User.findOne({ email }).then(dbUser => {
+              console.log(dbUser);
+              if(!dbUser){
+                  console.log("NEW USER");
+                  // create a new user!
+                  db.User.create({
+                      email,
+                      authType: "google",
+                      googleId: sub
+                  }).then(finalDbUser => {
+                      req.login(finalDbUser, () => {
+                          res.redirect(process.env.NODE_ENV === "production" ? "/" : "http://localhost:3000/");
+                      })
+                  }).catch(err => {
+                      console.log(err)
+                      res.sendStatus(500)
+                  })
+
+              } else {
+                  if(dbUser.authType === "google" && dbUser.googleId === sub + ""){
+                      req.login(dbUser, () => {
+                          res.redirect(process.env.NODE_ENV === "production" ? "/" : "http://localhost:3000/");
+                      })
+                  } else {
+                      res.sendStatus(500)
+                  }
+              }
+          }).catch(err => console.log(err))
+
+      }).catch(err => {
+          console.log(err)
+          res.sendStatus(500)
+      })
+  })
+})
+
+app.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 // Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/engauge";
@@ -23,3 +189,10 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
+
+process.on('SIGINT', () => {
+  mongoose.connection.close().then( () => {
+      console.log("Mongoose disconnected");
+      process.exit(0);
+  })
+})
