77376d3990db76cbf56cbfed2711553c791fe371
David Garza <dgarza0413@gmail.com>
2019-08-06 13:31:35 -0500

    git pull successful

--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -1,19 +1,6 @@
-<<<<<<< HEAD
 import React from "react";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
-=======
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-// import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
-import { borderRight } from '@material-ui/system';
->>>>>>> master
 
 
 function WellTable() {
@@ -33,11 +20,7 @@ function WellTable() {
         </thead>
         <tbody>
           <tr>
-            <td>Well 1</td>
-            <td>123-45-1235</td>
-            <td>Plunger Lift</td>
-            <td>Down the drain</td>
-            <td>On</td>
+
           </tr>
         </tbody>
       </Table>
