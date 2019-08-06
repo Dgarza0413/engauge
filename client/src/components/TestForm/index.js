ba282067b237aa5b6d7b6e1b4e892d7b90ddeef5
mbaker <mbaker.github@gmail.com>
2019-08-06 11:32:15 -0500

    api posts to database

--- client/src/components/TestForm/index.js
+++ client/src/components/TestForm/index.js
@@ -3,70 +3,80 @@ import API from "../../utils/API";
 
 class TestForm extends Component {
     state = {
-      apiNum: ""
-    }
-
+      apiNum: "",
+      wellNum: "",
+      wellName: ""
+    };
   
-
+    handleInputChange = event => {
+      const { name, value } = event.target;
+      this.setState({
+        [name]: value
+      });
+    };
+    
   
-
-  handleFormSubmit = event => {
-    event.preventDefault();
-    if (this.state.apiNum) {
-
-      const apiNum = this.state.apiNum.trim();
-
-      API.addWell(apiNum)
-        .then(res => {
-
-          console.log(res.data.items);
-
-          this.setState({
-            results: res.data.items
-          });
-        })
-        .catch(err => console.log(err));
+    handleFormSubmit = event => {
+      event.preventDefault();
+        const obj = {
+            apiNum: this.state.apiNum,
+            wellNum: this.state.wellNum,
+            wellName: this.state.wellName
+        }
+        API.addWell(obj)
+          .then(res => {
+  
+            console.log(res.data.items);
+  
+            this.setState({
+              obj: res.data.items
+            });
+          })
+          .catch(err => console.log(err));
+    };
+  
+    render() {
+      return (
+        <form onSubmit={this.handleFormSubmit}>
+            <div className="form-group">
+            <label htmlFor="apiNum">apiNum:</label>
+            <input
+                value={this.state.apiNum}
+                onChange={this.handleInputChange}
+                name="apiNum"
+                type="text"
+                className="form-control"
+                placeholder="apiNum"
+                id="apiNum"
+        />
+            <label htmlFor="wellNum">wellNum:</label>
+            <input
+                value={this.state.wellNum}
+                onChange={this.handleInputChange}
+                name="wellNum"
+                type="text"
+                className="form-control"
+                placeholder="wellNum"
+                id="wellNum"
+        />
+            <label htmlFor="wellName">wellName:</label>
+            <input
+                value={this.state.wellName}
+                onChange={this.handleInputChange}
+                name="wellName"
+                type="text"
+                className="form-control"
+                placeholder="wellName"
+                id="wellName"
+        />
+            <br />
+            <button className="btn btn-primary">
+              Search
+            </button>
+          </div>
+        </form>
+      );
     }
-  };
-
-  handleChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
   }
-
-  render() {
-    return (
-      <form onSubmit={this.handleFormSubmit}>
-        <div className="form-group">
-          <label>
-            API Number:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <label htmlFor="wellNum">wellNum:</label>
-          <input
-            value={this.state.wellNum}
-            onChange={this.handleInputChange}
-            name="wellNum"
-            type="text"
-            className="form-control"
-            placeholder=""
-            id="wellNum"
-          />
-          <label htmlFor="wellName">wellName:</label>
-          <input
-            value={this.state.wellName}
-            onChange={this.handleInputChange}
-            name="wellName"
-            type="text"
-            className="form-control"
-            placeholder=""
-            id="wellName"
-          />
-          <input type="submit" value="Submit" />
-        </div>
-      </form>
-    );
-  }
-}
 
 export default TestForm;
