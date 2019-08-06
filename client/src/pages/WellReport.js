5b5354bfcb15b6ebee494130798689483434a247
David Garza <dgarza0413@gmail.com>
2019-08-06 12:08:08 -0500

    well report changes

--- client/src/pages/WellReport.js
+++ client/src/pages/WellReport.js
@@ -1,9 +1,40 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
 
 class WellReport extends React.Component {
     render() {
         return (
-            <h1>This is the wellReport</h1>
+            <div>
+                <h1>This is the wellReport</h1>
+                <Card>
+                    <Card.Body>
+                        <Card.Subtitle className="mb-2 text-muted">Work summary title</Card.Subtitle>
+                        <Card.Text>
+                            Detail description of what happened
+                        </Card.Text>
+                        <Card.Text>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Text>
+                        <footer className="blockquote-footer">
+                            UserName
+                        </footer>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Body>
+                        <Card.Subtitle className="mb-2 text-muted">Work summary title</Card.Subtitle>
+                        <Card.Text>
+                            Detail description of what happened
+                        </Card.Text>
+                        <Card.Text>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Text>
+                        <footer className="blockquote-footer">
+                            UserName
+                        </footer>
+                    </Card.Body>
+                </Card>
+            </div>
         )
     }
 }
