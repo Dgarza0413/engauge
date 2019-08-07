2ec0c7150142d876dee278b77780c5102d22c3a3
Christina Truong <christinaqtruong@gmail.com>
2019-08-07 11:13:39 -0500

    created search bar

--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -1,14 +1,42 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
-// import Button from "react-bootstrap/Button";
 import { Link } from 'react-router-dom'
+import { MDBCol, MDBIcon } from "mdbreact";
+import "./style.css"
+import React, { Component, useRef, useEffect } from 'react';
 
 
 function WellTable(props) {
   
+  const SearchPage = () => {
+    return (
+      <MDBCol md="6">
+        <div className="input-group md-form form-sm form-1 pl-0">
+          <div className="input-group-prepend">
+            <span className="input-group-text purple lighten-3" id="basic-text1">
+              <MDBIcon className="text-white" icon="search" />
+            </span>
+          </div>
+          <input className="form-control my-0 py-1" type="text" placeholder="Search" aria-label="Search" />
+        </div>
+      </MDBCol>
+    );
+  }
+  
+  $("#submit").click(function() {
+    $(".element").each(function() {
+        if ($(this).attr("id") == $("#search").val()) {
+           $('html,body').animate({
+               scrollTop: $(this).offset().top
+           });
+        }
+    });
+});
+
   return (
     <div>
-      {/* <Button type="submit">Add Well</Button> */}
+    {SearchPage()}
+
 
       <Table striped bordered hover>
         <thead>
