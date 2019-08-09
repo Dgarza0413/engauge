11cfe7d69320d1638171d86ca2c97e532dc06312
Brittany McNulty <Brittany.m.mcnulty@gmail.com>
2019-08-09 12:51:34 -0500

    added controllers

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userController = require("../../controllers/userController");
 const wellController = require("../../controllers/wellController");
+const prodController = require("../../controllers/prodController");
+const recompletionController = require("../../controllers/recompletionController");
 const passport = require('../../config/passport.js')
 const { google } = require("googleapis")
 // const google = require("googleapis").google
