5ef57ffb9d6121e57ae94a3515dcaf8b12d2f3d4
elaintran <elain.ttran@gmail.com>
2019-08-11 19:22:22 -0500

    fixed form submit redirect and input change

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./style.css";
 
-const checkType = (type, content, mb, value) => {
+const checkType = (type, content, mb, value, width) => {
     if (type === "submit") {
-        return <input type={type} value={value} className="button" style={{marginBottom: mb || "40px"}} />
+        return <input type={type} value={value} className="button" style={{marginBottom: mb || "40px", width: width || ""}} />
     } else {
         return (
             <div className="button" style={{marginBottom: mb || "40px"}}>
@@ -15,7 +15,7 @@ const checkType = (type, content, mb, value) => {
 
 function Button(props) {
     return (
-        checkType(props.type, props.children, props.mb, props.value)
+        checkType(props.type, props.children, props.mb, props.value, props.width)
     );
 }
 
