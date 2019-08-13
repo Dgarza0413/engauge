e2bd189b7e23f3aa3549d753e852594525891dfa
elaintran <elain.ttran@gmail.com>
2019-08-13 10:07:18 -0500

    added overflow on nav

--- client/src/components/Drawer/index.js
+++ client/src/components/Drawer/index.js
@@ -59,60 +59,9 @@ class Drawer extends React.Component {
         }
     }
 
-    // displayOpen = () => {
-    //     return (
-    //         <div>
-    //             <div className="drawer-container slide-left">
-    //                 <i class="fas fa-bars toggle" onClick={() => this.handleToggle()}></i>
-    //                 <div className="navbar">
-    //                     <h4>Engauge</h4>
-    //                 </div>
-    //             </div>
-    //             <div className="menu nav-active">
-    //                 <div className="links">
-    //                     {links.map((text, index) => (
-    //                         <Link to={"/" + text.name.toLowerCase()} key={index}>
-    //                             <h6><i className={text.icon}></i>{text.name}</h6>
-    //                         </Link>
-    //                     ))}
-    //                     <Link to="/logout">
-    //                         <h6 className="logout"><i className="fas fa-sign-out-alt"></i>Logout</h6>
-    //                     </Link>
-    //                 </div>  
-    //             </div>
-    //         </div>
-    //     );
-    // }
-
-    // displayClosed = () => {
-    //     return (
-    //         <div>
-    //             <div className="drawer-container">
-    //                 <i class="fas fa-bars toggle" onClick={() => this.handleToggle()}></i>
-    //                 <div className="navbar">
-    //                     <h4>Engauge</h4>
-    //                 </div>
-    //             </div>
-    //             <div className="menu">
-    //                 <div className="links hide">
-    //                     {links.map((text, index) => (
-    //                         <Link to={"/" + text.name.toLowerCase()} key={index}>
-    //                             <h6><i className={text.icon}></i>{text.name}</h6>
-    //                         </Link>
-    //                     ))}
-    //                     <Link to="/logout">
-    //                         <h6 className="logout"><i className="fas fa-sign-out-alt"></i>Logout</h6>
-    //                     </Link>
-    //                 </div>  
-    //             </div>
-    //         </div>
-    //     );
-    // }
-
     render() {
         return (
-                // (this.state.toggle === false) ? this.displayOpen() : this.displayClosed()  
-            <div>
+            <div style={{overflow: "hidden"}}>
                 <div className={this.state.display.left}>
                     <div className="navbar">
                         <i class="fas fa-bars toggle" onClick={() => this.handleToggle()}></i>
