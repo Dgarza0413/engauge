270212b38467242fe63a6792cb4a6f25c671ca14
David Garza <dgarza0413@gmail.com>
2020-05-24 23:54:12 -0500

    useCombinevalue hook placed in dashbroad

--- client/src/hooks/useCombineValue.js
+++ client/src/hooks/useCombineValue.js
@@ -1,68 +1,33 @@
 import { useState } from "react";
+import API from '../utils/API';
+import moment from 'moment'
 
-const useCombineValue = () => {
+const useCombineValue = (data, setReportData) => {
+    const newObj = [];
 
-    const [value, setValue] = useState({})
-
-    API.getAllProdData()
-        .then(res => {
-            const obj = res.data;
-            const newObj = [];
-            for (let i = 0; i < obj.length; i++) {
-                const date = moment(obj[i].date).format("MM/DD/YYYY");
-                if (!newObj[date]) {
-                    newObj[date] =
-                    {
-                        date: date,
-                        water: [],
-                        oil: [],
-                        gas: []
-                    }
-                    newObj[date].oil.push(obj[i].oil)
-                    newObj[date].gas.push(obj[i].gas)
-                    newObj[date].water.push(obj[i].water)
-                } else {
-                    newObj[date].oil.push(obj[i].oil)
-                    newObj[date].gas.push(obj[i].gas)
-                    newObj[date].water.push(obj[i].water)
-                }
+    for (var i = 0; i < data.length; i++) {
+        const day = moment(data[i].date).format('YYYY-MM-DD');
+        if (!newObj[day]) {
+            newObj[day] = {
+                day: day,
+                value: []
             }
-            for (let key in newObj) {
-                let gas = newObj[key].gas
-                let oil = newObj[key].oil
-                let water = newObj[key].water
-                let totalGas = gas.reduce((acc, cur) => acc + cur)
-                let totalOil = oil.reduce((acc, cur) => acc + cur)
-                let totalWater = water.reduce((acc, cur) => acc + cur)
-                newObj[key].gas = totalGas
-                newObj[key].oil = totalOil
-                newObj[key].water = totalWater
-            }
-            const objValue = Object.values(newObj)
-            this.setState({
-                wellData: objValue,
-                currentProd: objValue[objValue.length - 1],
-            })
-        })
-        .catch(err => console.log(err))
-
-    const handleInputChange = (event) => {
-        event.persist()
-        setValue(values => {
-            return {
-                ...values,
-                [event.target.name]: event.target.value
-            }
-        })
+            newObj[day].value.push(1)
+        } else {
+            newObj[day].value.push(1)
+        }
     }
 
-    const handleBind = (event) => {
-        setValue(event)
+    for (let key in newObj) {
+        let value = newObj[key].value
+        let day = newObj[key].day
+        let totalValue = value.reduce((acc, cur) => acc + cur);
+        newObj[key].day = day;
+        newObj[key].value = totalValue;
     }
+    console.log(newObj)
 
-
-    return [value, handleInputChange, handleBind]
+    setReportData(Object.values(newObj))
 }
 
-export default useInputChange;
-
+export default useCombineValue
