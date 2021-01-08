160988e50ab94e24c49658100d9a6a9153bcba03
David Garza <dgarza0413@gmail.com>
2021-01-08 13:39:10 -0600

    clean up on models file

--- models/index.js
+++ models/index.js
@@ -1,14 +1,7 @@
 module.exports = {
-<<<<<<< HEAD
   User: require("./user"),
   Well: require("./well"),
   Production: require("./production"),
-  Recompletion: require("./recompletion")
-=======
-    User: require("./user"),
-    Well: require("./well"),
-    Production: require("./production"),
-    Recompletion: require("./recompletion"),
-    Report: require("./report")
->>>>>>> 842c7e8a6948f1e7a987a25e8757b47ad7a5626c
+  Recompletion: require("./recompletion"),
+  Report: require("./report")
 };
